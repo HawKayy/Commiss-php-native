8fa52738c0bb459e29b29e58a74bc163b561e497
HawKayy <takanomenokayy@gmail.com>
2022-01-16 12:05:20 +0700

    Major Update!

--- login.php
+++ login.php
@@ -41,7 +41,8 @@
         $_SESSION ['id'] = $d->admin_id;
         echo '<script>window.location="dashboard.php"</script>';
     } else{
-        echo '<script>alert(mysql_error($cek)</script>';
+        echo mysql_error($conn);
+        echo mysql_error($cek);
     }
         }
 ?>
